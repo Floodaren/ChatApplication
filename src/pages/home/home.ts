5ef720d45afdb7e7260939e372fdf0574aa4beec
flodare <Nicholas.flod@gmail.com>
2017-11-07 14:29:51 +0100

    First commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  x = 3;
+  
   constructor(public navCtrl: NavController) {
+    var sha256 = CryptoJS.SHA256("hejsan");
+    var sha2562 = CryptoJS.SHA256("hejsadsan");
+    var matchorNot:boolean = false;
+    
+    for (var i = 0; i<sha256.words.length;i++)
+    {
+      if (sha256.words[i] == sha2562.words[i])
+      {
+        matchorNot = true;
+      }
+      else 
+      {
+        matchorNot = false;
+      }
+    }
+    if (matchorNot == true)
+    {
+      console.log("inloggad");
+    }
+    
 
+    var hejsan = CryptoJS.AES.encrypt('haha',"myKey");
+    var decryptText =CryptoJS.AES.decrypt(hejsan.toString(), 'myKey');
+    this.x = decryptText.toString(CryptoJS.enc.Utf8)
   }
 
 }
